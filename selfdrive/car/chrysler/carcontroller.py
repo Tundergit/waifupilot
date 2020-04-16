115642881a67e9f60c8a4da8c60c31664947708e
Tunder (Chris in RL) <34691234+Tundergit@users.noreply.github.com>
2020-04-16 12:26:12 -0700

    Chrysler 200 port

--- selfdrive/car/chrysler/carcontroller.py
+++ selfdrive/car/chrysler/carcontroller.py
@@ -1,7 +1,6 @@
 from selfdrive.car import apply_toyota_steer_torque_limits
-from selfdrive.car.chrysler.chryslercan import create_lkas_hud, create_lkas_command, \
-                                               create_wheel_buttons
-from selfdrive.car.chrysler.values import CAR, SteerLimitParams
+from selfdrive.car.chrysler.chryslercan import create_lkas_command, create_wheel_buttons
+from selfdrive.car.chrysler.values import CAR, DBC, SteerLimitParams
 from opendbc.can.packer import CANPacker
 
 class CarController():
@@ -16,12 +15,13 @@ class CarController():
     self.gone_fast_yet = False
     self.steer_rate_limited = False
 
-    self.packer = CANPacker(dbc_name)
+    self.packer = CANPacker(DBC[CP.carFingerprint]['pt'])
 
 
-  def update(self, enabled, CS, actuators, pcm_cancel_cmd, hud_alert):
+  def update(self, enabled, CS, actuators, pcm_cancel_cmd): #TODO hud_alert
     # this seems needed to avoid steering faults and to force the sync with the EPS counter
     frame = CS.lkas_counter
+    ccframe = CS.frame_23b
     if self.prev_frame == frame:
       return []
 
@@ -35,9 +35,9 @@ class CarController():
     moving_fast = CS.out.vEgo > CS.CP.minSteerSpeed  # for status message
     if CS.out.vEgo > (CS.CP.minSteerSpeed - 0.5):  # for command high bit
       self.gone_fast_yet = True
-    elif self.car_fingerprint in (CAR.PACIFICA_2019_HYBRID, CAR.JEEP_CHEROKEE_2019):
-      if CS.out.vEgo < (CS.CP.minSteerSpeed - 3.0):
-        self.gone_fast_yet = False  # < 14.5m/s stock turns off this bit, but fine down to 13.5
+  #  elif self.car_fingerprint in (CAR.PACIFICA_2019_HYBRID, CAR.JEEP_CHEROKEE_2019):
+  #    if CS.out.vEgo < (CS.CP.minSteerSpeed - 3.0):
+  #      self.gone_fast_yet = False  # < 14.5m/s stock turns off this bit, but fine down to 13.5
     lkas_active = moving_fast and enabled
 
     if not lkas_active:
@@ -56,13 +56,14 @@ class CarController():
 
     # LKAS_HEARTBIT is forwarded by Panda so no need to send it here.
     # frame is 100Hz (0.01s period)
-    if (self.ccframe % 25 == 0):  # 0.25s period
-      if (CS.lkas_car_model != -1):
-        new_msg = create_lkas_hud(
-            self.packer, CS.out.gearShifter, lkas_active, hud_alert,
-            self.hud_count, CS.lkas_car_model)
-        can_sends.append(new_msg)
-        self.hud_count += 1
+    #TODO LKAS_HUD
+  #  if (self.ccframe % 25 == 0):  # 0.25s period
+  #    if (CS.lkas_car_model != -1):
+  #      new_msg = create_lkas_hud(
+  #          self.packer, CS.out.gearShifter, lkas_active, hud_alert,
+  #          self.hud_count, CS.lkas_car_model)
+  #      can_sends.append(new_msg)
+  #      self.hud_count += 1
 
     new_msg = create_lkas_command(self.packer, int(apply_steer), self.gone_fast_yet, frame)
     can_sends.append(new_msg)
