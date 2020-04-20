554e052e300b3fee801359f786601ded23897728
Jason Young <jyoung8607@gmail.com>
2020-04-20 00:41:45 -0400

    MISRA

--- panda/board/safety/safety_chrysler.h
+++ panda/board/safety/safety_chrysler.h
@@ -87,7 +87,7 @@ static int chrysler_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
 
     // enter controls on rising edge of ACC, exit controls on ACC off
     if (addr == 1006) {
-      int cruise_engaged = (GET_BYTE(to_push, 0) >> 5) & 0x7 == 4;
+      int cruise_engaged = ((GET_BYTE(to_push, 0) >> 5) & 0x7) == 4;
       if (cruise_engaged && !chrysler_cruise_engaged_last) {
         controls_allowed = 1;
       }
