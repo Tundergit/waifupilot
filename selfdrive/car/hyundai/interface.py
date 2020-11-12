32a8159c7c3c1a12dda2b28c94f31e54634bddac
Tunder (Chris in RL) <34691234+Tundergit@users.noreply.github.com>
2020-11-11 23:01:34 -0800

    controlsd crash fix
    c.left, c.right_blinker

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -229,6 +229,7 @@ class CarInterface(CarInterfaceBase):
   def apply(self, c):
     can_sends = self.CC.update(c.enabled, self.CS, self.frame, c.actuators,
                                c.cruiseControl.cancel, c.hudControl.visualAlert, c.hudControl.leftLaneVisible,
-                               c.hudControl.rightLaneVisible, c.hudControl.leftLaneDepart, c.hudControl.rightLaneDepart)
+                               c.hudControl.rightLaneVisible, c.hudControl.leftLaneDepart, c.hudControl.rightLaneDepart,
+                               c.left_blinker, c.right_blinker)
     self.frame += 1
     return can_sends
