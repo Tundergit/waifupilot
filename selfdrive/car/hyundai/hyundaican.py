cb414c2967fdceadb8012f853e05f10be180025c
Tunder (Chris in RL) <34691234+Tundergit@users.noreply.github.com>
2020-11-14 15:49:12 -0800

    add turn sig 360 can

--- selfdrive/car/hyundai/hyundaican.py
+++ selfdrive/car/hyundai/hyundaican.py
@@ -1,4 +1,5 @@
 import crcmod
+from cereal import car
 from selfdrive.car.hyundai.values import CAR, CHECKSUM
 
 hyundai_checksum = crcmod.mkCrcFun(0x11D, initCrc=0xFD, rev=False, xorOut=0xdf)
@@ -87,3 +88,23 @@ def create_lfa_mfa(packer, frame, enabled):
   # HDA_USM: nothing
 
   return packer.make_can_msg("LFAHDA_MFC", 0, values)
+
+TurnSigLh = car.CarState.CF_Gway_TSigLHSw
+TurnSigRh = car.CarState.CF_Gway_TSigRHSw
+
+def create_360_lane_view(packer, frame, TurnSigLh, TurnSigRh, dat):
+  if TurnSigLh != 0:
+    if frame % 51 == 0:
+      dat = b'\x02\x10\x03\x00\x00\x00\x00\x00'   
+    if frame % 299 == 0:
+      dat = b'\x02\x3E\x00\x00\x00\x00\x00\x00'    
+    if frame % 101 == 0:
+      dat = b'\x05\x2F\xF0\x24\x07\xFF\x00\x00'
+  if TurnSigRh != 0:
+    if frame % 51 == 0:
+      dat = b'\x02\x10\x03\x00\x00\x00\x00\x00'   
+    if frame % 299 == 0:
+      dat = b'\x02\x3E\x00\x00\x00\x00\x00\x00'    
+    if frame % 101 == 0:
+      dat = b'\x05\x2F\xF0\x24\x07\xFF\x00\x00'
+  return make_can_msg(0x793, dat, 0)
