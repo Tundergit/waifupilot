0c8a18cabafb2e130f8dd0e4db714fbc9130230e
Tunder (Chris in RL) <34691234+Tundergit@users.noreply.github.com>
2020-11-29 01:53:01 -0800

    add loops

--- selfdrive/ui/sound.hpp
+++ selfdrive/ui/sound.hpp
@@ -6,14 +6,14 @@ typedef cereal::CarControl::HUDControl::AudibleAlert AudibleAlert;
 
 static std::map<AudibleAlert, std::pair<const char *, int>> sound_map {
   // AudibleAlert, (file path, loop count)
-  {AudibleAlert::CHIME_DISENGAGE, {"../assets/sounds/disengaged.wav", 0}},
-  {AudibleAlert::CHIME_ENGAGE, {"../assets/sounds/engaged.wav", 0}},
-  {AudibleAlert::CHIME_WARNING1, {"../assets/sounds/warning_1.wav", 0}},
-  {AudibleAlert::CHIME_WARNING2, {"../assets/sounds/warning_2.wav", 0}},
+  {AudibleAlert::CHIME_DISENGAGE, {"../assets/sounds/disengaged.wav", 1}},
+  {AudibleAlert::CHIME_ENGAGE, {"../assets/sounds/engaged.wav", 1}},
+  {AudibleAlert::CHIME_WARNING1, {"../assets/sounds/warning_1.wav", 1}},
+  {AudibleAlert::CHIME_WARNING2, {"../assets/sounds/warning_2.wav", 1}},
   {AudibleAlert::CHIME_WARNING2_REPEAT, {"../assets/sounds/warning_2.wav", 3}},
   {AudibleAlert::CHIME_WARNING_REPEAT, {"../assets/sounds/warning_repeat.wav", -1}},
-  {AudibleAlert::CHIME_ERROR, {"../assets/sounds/error.wav", 0}},
-  {AudibleAlert::CHIME_PROMPT, {"../assets/sounds/error.wav", 0}}
+  {AudibleAlert::CHIME_ERROR, {"../assets/sounds/error.wav", 1}},
+  {AudibleAlert::CHIME_PROMPT, {"../assets/sounds/error.wav", 1}}
 };
 
 class Sound {
