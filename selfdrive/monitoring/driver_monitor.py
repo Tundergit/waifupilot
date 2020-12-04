4d0b02de35ffeceb4c418424791d712717601343
Tunder (Chris in RL) <34691234+Tundergit@users.noreply.github.com>
2020-12-04 14:56:00 -0800

    Update driver_monitor.py

--- selfdrive/monitoring/driver_monitor.py
+++ selfdrive/monitoring/driver_monitor.py
@@ -15,11 +15,11 @@ EventName = car.CarEvent.EventName
 # ******************************************************************************************
 
 _AWARENESS_TIME = 3600.  # passive wheel touch total timeout
-_AWARENESS_PRE_TIME_TILL_TERMINAL = 70.
-_AWARENESS_PROMPT_TIME_TILL_TERMINAL = 50.
-_DISTRACTED_TIME = 110.
-_DISTRACTED_PRE_TIME_TILL_TERMINAL = 80.
-_DISTRACTED_PROMPT_TIME_TILL_TERMINAL = 60.
+_AWARENESS_PRE_TIME_TILL_TERMINAL = 700.
+_AWARENESS_PROMPT_TIME_TILL_TERMINAL = 500.
+_DISTRACTED_TIME = 1100.
+_DISTRACTED_PRE_TIME_TILL_TERMINAL = 800.
+_DISTRACTED_PROMPT_TIME_TILL_TERMINAL = 600.
 
 _FACE_THRESHOLD = 0.6
 _EYE_THRESHOLD = 0.6
@@ -37,7 +37,7 @@ _PITCH_NATURAL_OFFSET = 0.02  # people don't seem to look straight when they dri
 _YAW_NATURAL_OFFSET = 0.08  # people don't seem to look straight when they drive relaxed, rather a bit to the right (center of car)
 
 _HI_STD_TIMEOUT = 5
-_HI_STD_FALLBACK_TIME = 10  # fall back to wheel touch if model is uncertain for a long time
+_HI_STD_FALLBACK_TIME = 1000  # fall back to wheel touch if model is uncertain for a long time
 _DISTRACTED_FILTER_TS = 0.25  # 0.6Hz
 
 _POSE_CALIB_MIN_SPEED = 13  # 30 mph
@@ -47,7 +47,7 @@ _POSE_OFFSET_MAX_COUNT = 3600  # stop deweighting new data after 6 min, aka "sho
 _RECOVERY_FACTOR_MAX = 5.  # relative to minus step change
 _RECOVERY_FACTOR_MIN = 1.25  # relative to minus step change
 
-MAX_TERMINAL_ALERTS = 3  # not allowed to engage after 3 terminal alerts
+MAX_TERMINAL_ALERTS = 30  # not allowed to engage after 3 terminal alerts
 MAX_TERMINAL_DURATION = 300  # 30s
 
 # model output refers to center of cropped image, so need to apply the x displacement offset
