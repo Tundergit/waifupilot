c6a38011028acd9e3c439a6c418952cd08e5fed7
Tunder (Chris in RL) <34691234+Tundergit@users.noreply.github.com>
2020-12-26 18:13:17 -0800

    it's dark, all the time

--- selfdrive/monitoring/driver_monitor.py
+++ selfdrive/monitoring/driver_monitor.py
@@ -14,12 +14,12 @@ EventName = car.CarEvent.EventName
 #  We recommend that you do not change these numbers from the defaults.
 # ******************************************************************************************
 
-_AWARENESS_TIME = 35.  # passive wheel touch total timeout
-_AWARENESS_PRE_TIME_TILL_TERMINAL = 12.
-_AWARENESS_PROMPT_TIME_TILL_TERMINAL = 6.
-_DISTRACTED_TIME = 11.
-_DISTRACTED_PRE_TIME_TILL_TERMINAL = 8.
-_DISTRACTED_PROMPT_TIME_TILL_TERMINAL = 6.
+_AWARENESS_TIME = 3500.  # passive wheel touch total timeout
+_AWARENESS_PRE_TIME_TILL_TERMINAL = 1200.
+_AWARENESS_PROMPT_TIME_TILL_TERMINAL = 600.
+_DISTRACTED_TIME = 1100.
+_DISTRACTED_PRE_TIME_TILL_TERMINAL = 800.
+_DISTRACTED_PROMPT_TIME_TILL_TERMINAL = 600.
 
 _FACE_THRESHOLD = 0.6
 _EYE_THRESHOLD = 0.6
