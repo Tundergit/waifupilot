2a4b7b853207943286ca311f932bd1a64bbb7146
Tunder (Chris in RL) <34691234+Tundergit@users.noreply.github.com>
2021-03-08 14:21:41 -0800

    debug

--- opendbc/can/parser.cc
+++ opendbc/can/parser.cc
@@ -134,8 +134,8 @@ CANParser::CANParser(int abus, const std::string& dbc_name,
       }
     }
     if (!msg) {
-      fprintf(stderr, "CANParser: could not find message 0x%X in DBC %s\n", op.address, dbc_name.c_str());
-      assert(false);
+      fprintf(stderr, "CANParser: could not find message 0x%[i] in DBC %s\n", op.address, dbc_name.c_str());
+      static_assert(false);
     }
 
     state.size = msg->size;
