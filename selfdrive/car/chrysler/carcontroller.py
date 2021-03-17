3dc96e83af116475736735c9c1e867734056f8a0
Tunder (Chris in RL) <34691234+Tundergit@users.noreply.github.com>
2021-03-17 07:50:31 -0700

    Update carcontroller.py

--- selfdrive/car/chrysler/carcontroller.py
+++ selfdrive/car/chrysler/carcontroller.py
@@ -13,12 +13,17 @@ class CarController():
     self.hud_count = 0
     self.car_fingerprint = CP.carFingerprint
     self.steer_rate_limited = False
+    self.prev_frame = -1
 
     self.packer = CANPacker(dbc_name)
 
   def update(self, enabled, CS, frame, actuators, pcm_cancel_cmd, leftLaneVisible,
              rightLaneVisible):  # TODO hud_alert
     P = CarControllerParams
+    
+    frame = CS.lkas_counter
+    if self.prev_frame == frame:
+      return []
 
     steer_ready = CS.out.vEgo > CS.CP.minSteerSpeed + 0.2
     steer_command_bit = CS.out.vEgo > CS.CP.minSteerSpeed + 0.1
@@ -48,5 +53,7 @@ class CarController():
 
     if frame % P.HUD_STEP == 0:
       can_sends.append(create_lkas_hud(self.packer, enabled, leftLaneVisible, rightLaneVisible))
+    
+    self.prev_frame = frame 
 
     return can_sends
