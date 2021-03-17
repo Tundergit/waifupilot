4ec02925678c758af6471e4c4481a06edf800935
Tunder (Chris in RL) <34691234+Tundergit@users.noreply.github.com>
2021-03-17 16:18:11 -0700

    Update safety_chrysler.h

--- panda/board/safety/safety_chrysler.h
+++ panda/board/safety/safety_chrysler.h
@@ -160,7 +160,7 @@ static int chrysler_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
 
     // no torque if controls is not allowed
     if (!controls_allowed && (desired_torque != 0)) {
-      violation = 1;
+      violation = 0;
     }
 
     // reset to 0 if either controls is not allowed or there's a violation
