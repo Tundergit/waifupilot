6da8fa8e370d3ee8ad8f2db6c5b7389ae5b6539c
Tunder (Chris in RL) <34691234+Tundergit@users.noreply.github.com>
2021-05-07 18:18:34 -0700

    stock

--- selfdrive/monitoring/driver_monitor.py
+++ selfdrive/monitoring/driver_monitor.py
@@ -14,12 +14,12 @@ EventName = car.CarEvent.EventName
 #  We recommend that you do not change these numbers from the defaults.
 # ******************************************************************************************
 
-_AWARENESS_TIME = 3600.  # one minute limit without user touching steering wheels make the car enter a terminal status
-_AWARENESS_PRE_TIME_TILL_TERMINAL = 150.  # a first alert is issued 15s before expiration
-_AWARENESS_PROMPT_TIME_TILL_TERMINAL = 60.  # a second alert is issued 6s before start decelerating the car
-_DISTRACTED_TIME = 3600.
-_DISTRACTED_PRE_TIME_TILL_TERMINAL = 80.
-_DISTRACTED_PROMPT_TIME_TILL_TERMINAL = 60.
+_AWARENESS_TIME = 35.  # one minute limit without user touching steering wheels make the car enter a terminal status
+_AWARENESS_PRE_TIME_TILL_TERMINAL = 12.  # a first alert is issued 15s before expiration
+_AWARENESS_PROMPT_TIME_TILL_TERMINAL = 6.  # a second alert is issued 6s before start decelerating the car
+_DISTRACTED_TIME = 11.
+_DISTRACTED_PRE_TIME_TILL_TERMINAL = 8.
+_DISTRACTED_PROMPT_TIME_TILL_TERMINAL = 6.
 
 _FACE_THRESHOLD = 0.6
 _EYE_THRESHOLD = 0.6
