975fb442c4920c78ed79d72c3dda50fa22d1e6d6
Tunder (Chris in RL) <34691234+Tundergit@users.noreply.github.com>
2025-01-10 18:54:13 -0800

    Update launch_chffrplus.sh
    undo long shot (that didn't work anyway.)

--- launch_chffrplus.sh
+++ launch_chffrplus.sh
@@ -71,13 +71,13 @@ function launch {
   fi
 
   # Check for NEOS update
-  # if [ $(< /VERSION) != "14" ]; then
-   # if [ -f "$DIR/scripts/continue.sh" ]; then
-    #  cp "$DIR/scripts/continue.sh" "/data/data/com.termux/files/continue.sh"
-   # fi
+  if [ $(< /VERSION) != "14" ]; then
+    if [ -f "$DIR/scripts/continue.sh" ]; then
+      cp "$DIR/scripts/continue.sh" "/data/data/com.termux/files/continue.sh"
+    fi
 
-   # "$DIR/installer/updater/updater" "file://$DIR/installer/updater/update.json"
-  # fi
+    "$DIR/installer/updater/updater" "file://$DIR/installer/updater/update.json"
+   fi
 
 
   # handle pythonpath
