c330cfdb65409f3226052e25606941030713bdf5
Tunder (Chris in RL) <34691234+Tundergit@users.noreply.github.com>
2025-03-01 15:24:27 -0800

    Update manager.py
    skip accepting things

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -505,12 +505,12 @@ def main():
   params.manager_start()
 
   default_params = [
-    ("CommunityFeaturesToggle", "0"),
-    ("CompletedTrainingVersion", "0"),
+    ("CommunityFeaturesToggle", "1"),
+    ("CompletedTrainingVersion", "1"),
     ("IsMetric", "0"),
     ("RecordFront", "0"),
-    ("HasAcceptedTerms", "0"),
-    ("HasCompletedSetup", "0"),
+    ("HasAcceptedTerms", "1"),
+    ("HasCompletedSetup", "1"),
     ("IsUploadRawEnabled", "1"),
     ("IsLdwEnabled", "1"),
     ("IsGeofenceEnabled", "-1"),
